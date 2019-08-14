fee050753c940feca66933af8ad2e4f739f8c5b6
767312562 <45227093+767312562@users.noreply.github.com>
2019-08-14 16:04:58 +0800

    Update lmu_pub.py

--- src/navigation/src/lmu_pub.py
+++ src/navigation/src/lmu_pub.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 '''lmu ROS Node'''
+
 # license removed for brevity
+# encoding:utf-8
+
 import rospy
 from std_msgs.msg import Float64
 import serial
@@ -9,64 +12,71 @@ from math import sin, cos, atan
 import json
 import fileinput
 
+
+
 def talker():
     '''lmu Publisher'''
     pub = rospy.Publisher('/course_real', Float64, queue_size=10)
     rospy.init_node('lmu', anonymous=True)
     rate = rospy.Rate(10) # 10hz
-    try:
-        ser=serial.Serial('/dev/ttyACM0',9600)
-    except Exception:
-        print 'open serial failed.'
-        exit(1)
 
-    while True:
-        s = ser.readline()
-        am=str(s).strip().split(" ")
-        if len(am)<14:
-            continue
-        else:
-            a_x=float(am[2])
-            a_y=float(am[4])
-            a_z=float(am[6])
-            m_x=float(am[9])
-            m_y=float(am[11])
-            m_z=float(am[13])
+    while not rospy.is_shutdown():
+        try:
+            ser=serial.Serial('/dev/ttyACM0',9600)
+        except Exception:
+            print ('open serial failed.')
+            exit(1)
 
-            #计算航向
-            eta=atan(a_x/(np.sqrt(np.square(a_y)+np.square(a_z))))
-            thita=atan(a_y/(np.sqrt(np.square(a_x)+np.square(a_z))))
-            Hy=m_y*cos(thita)+m_x*sin(thita)*sin(eta)-m_z*cos(eta)*sin(thita)
-            Hx=m_x*cos(eta)+m_z*sin(eta)
-            phi=atan(Hy/Hx)
-            #滤波程序
-            phi_save='phi.json'
-            with open(phi_save,'w') as phi_obj:
-                json.dump(phi,phi_obj)
-            count = len(open(phi_save, 'r').readlines())
-            if count <200:
+        while True:
+            s = ser.readline()
+            am=str(s).strip().split(" ")
+            if len(am)<14:
                 continue
             else:
-                for line in fileinput.input('phi.json', inplace=1):
-                    if not fileinput.isfirstline():
-                        print(line.replace('\n',''))
+                a_x=float(am[2])
+                a_y=float(am[4])
+                a_z=float(am[6])
+                m_x=float(am[9])
+                m_y=float(am[11])
+                m_z=float(am[13])
+                eta=atan(a_x/(np.sqrt(np.square(a_y)+np.square(a_z))))
+                thita=atan(a_y/(np.sqrt(np.square(a_x)+np.square(a_z))))
+                Hy=m_y*cos(thita)+m_x*sin(thita)*sin(eta)-m_z*cos(eta)*sin(thita)
+                Hx=m_x*cos(eta)+m_z*sin(eta)
+                phi=atan(Hy/Hx)
 
-            phi_read=[]
-            with open(phi_save) as f:
-                for line in f:
-                    phi_read.append(line.strip('\n'))
-            phi_read = list(map(float, phi_read))
-            if len(phi_read) < 20:
-                phi_publish=phi
-            else:
-                phi_read.reverse()
-                phi_filter=phi_read[0:19]
-                phi_filter.remove(max(phi_filter))
-                phi_filter.remove(min(phi_filter))
-                phi_publish=sum(phi_filter)/len(phi_filter)
+                phi_save='phi.json'
+                with open(phi_save,'a') as phi_obj:
+                    phi_obj.write('\n'+str(phi))
+                count = len(open(phi_save, 'r').readlines())
+                if count <200:
+                    pass
+                else:
+                    for line in fileinput.input('phi.json', inplace=1):
+                        if not fileinput.isfirstline():
+                            print(line.replace('\n',''))
+                    for line in fileinput.input('phi.json', inplace=1):
+                        if not fileinput.isfirstline():
+                            print(line.replace('\n',''))
 
-            while not rospy.is_shutdown():
+                phi_read=[]
+                with open(phi_save) as f:
+                    for line in f:
+                        if line.count('\n')==len(line):
+                            pass
+                        else:
+                            phi_read.append(line.strip('\n'))
+                phi_read = list(map(float, phi_read))
+                if len(phi_read) < 20:
+                    phi_publish=phi
+                else:
+                    phi_read.reverse()
+                    phi_filter=phi_read[0:19]
+                    phi_filter.remove(max(phi_filter))
+                    phi_filter.remove(min(phi_filter))
+                    phi_publish=sum(phi_filter)/len(phi_filter)
                 pub.publish(phi_publish)
+                rospy.loginfo(phi_publish)
                 rate.sleep()
 
 if __name__ == '__main__':
